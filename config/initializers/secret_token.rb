0019c83028322c8664b09aa9c387973f292371ed
Antonio Medina <amedina.emergya@gmail.com>
2011-03-17 20:42:14 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyart::Application.config.secret_token = 'ac4c277fefaf99d9362aa9f5380ca0eda7c81ba31c6075b588ad9c0761763c56fa5d7398b37bf06f214b50d874c4f69221d3200253a81cb2d37b82fc233a3f76'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rubyart::Application.config.session_store :cookie_store, :key => '_rubyart_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rubyart::Application.config.session_store :active_record_store
